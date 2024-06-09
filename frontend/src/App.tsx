34635a0103a65edac22040afebdfb69e8ef3bae8
antonAtananasov <anton_atananasov@abv.bg>
2024-06-09 20:31:20 +0300

    fixed app.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,47 +1,61 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Button, Form, Container, Row, Col, Nav, Navbar } from 'react-bootstrap';
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Button, Nav, Navbar } from "react-bootstrap";
 import RegisterAccountForm from './components/RegisterAccountForm';
-import LoginForm from './components/RegisterAccountForm';
-import SignupForm from './components/RegisterAccountForm';
-
-
-
-
-const testGet = async (setter: React.Dispatch<React.SetStateAction<string>>) => {
-    const result = await fetch('http://localhost:3000/')
-    const text = await result.text()
-    setter(text)
-}
 
 
 const App = () => {
     const [isLogin, setIsLogin] = useState(true);
 
     return (
-        <Container fluid className="custom-container">
-            <Row className="justify-content-md-center">
-                <Col md="4">
-                    {isLogin ? <LoginForm /> : <SignupForm />}
-                    <div className="d-grid gap-2 mt-3">
-                        <Button
-                            variant={isLogin ? "primary" : "light"}
-                            onClick={() => setIsLogin(true)}
-                        >
-                            Login
-                        </Button>
-                        <Button
-                            variant={!isLogin ? "primary" : "light"}
-                            onClick={() => setIsLogin(false)}
-                        >
-                            Sign Up
-                        </Button>
-                    </div>
-                </Col>
-            </Row>
-        </Container>
+        <>
+            {/* put the BrowserRouter and Route tags here */}
+            <Navbar bg="dark" data-bs-theme="dark">
+                <Container>
+                    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+                    <Nav className="me-auto">
+                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="#features">Features</Nav.Link>
+                        <Nav.Link href="#pricing">Pricing</Nav.Link>
+                    </Nav>
+                </Container>
+            </Navbar>
+
+            <Container data-bs-theme="light position" fluid className="w-100">
+                <Row className="w-100 mt-4 mb-4">
+                    <Col className='col-2 border-end'>Left panel</Col>
+                    <Col className='col-8'>
+                        <Row className='w-100 p-3'>
+                            <Container fluid className="custom-container">
+                                <Row className="justify-content-md-center">
+                                    <Col md="4">
+                                        <RegisterAccountForm isLogin={isLogin} />
+                                        <div className="d-grid gap-2 mt-3">
+                                            <Button
+                                                variant={isLogin ? "primary" : "light"}
+                                                onClick={() => setIsLogin(true)}>
+                                                Login
+                                            </Button>
+                                            <Button
+                                                variant={!isLogin ? "primary" : "light"}
+                                                onClick={() => setIsLogin(false)}>
+                                                Sign Up
+                                            </Button>
+                                        </div>
+                                    </Col>
+                                </Row>
+                            </Container>
+                        </Row>
+                    </Col>
+                    <Col className='col-2 border-start'>Right panel</Col>
+                </Row>
+                <Row className='w-100 p-3 border-top'>Footer</Row>
+
+            </Container>
+            {/*  */}
+
+        </>
     );
 };
 
