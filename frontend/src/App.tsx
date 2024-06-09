0545d50c91a73e18f70d21fc230aaad55532f67b
HelinAlev389 <haley.alev>
2024-06-09 18:45:57 +0300

    Bootstrap form

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,19 @@
+<<<<<<< Updated upstream
+=======
+import { useEffect, useState } from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Nav, Navbar } from 'react-bootstrap';
+import { Container, Row, Col } from "react-bootstrap";
+import RegisterAccountForm from './components/RegisterAccountForm';
+
+
+const testGet = async (setter: React.Dispatch<React.SetStateAction<string>>) => {
+    const result = await fetch('http://localhost:3000/')
+    const text = await result.text()
+    setter(text)
+}
+>>>>>>> Stashed changes
 
 import './App.css';
 import LoginSignup from "./components/LoginSignup.tsx";
@@ -7,6 +23,7 @@ function App() {
   return (
 
 
+<<<<<<< Updated upstream
         <Layout>
             <LoginSignup/>
         </Layout>
@@ -14,6 +31,25 @@ function App() {
 
 
   );
+=======
+            <Container data-bs-theme="light position" fluid className="w-100">
+                <Row className="w-100 mt-4 mb-4">
+                    <Col className='col-2 border-end'>Left panel</Col>
+                    <Col className='col-8'>
+                        <Row className='w-100 p-3 d-flex text-start'>
+                            <RegisterAccountForm></RegisterAccountForm>
+                        </Row>
+                    </Col>
+                    <Col className='col-2 border-start'>Right panel</Col>
+                </Row>
+                <Row className='w-100 p-3 border-top'>Footer</Row>
+
+            </Container> {/*
+ */}
+            {/*  */}
+        </>
+    )
+>>>>>>> Stashed changes
 }
 
 export default App;
